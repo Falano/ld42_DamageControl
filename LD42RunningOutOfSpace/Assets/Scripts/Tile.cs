ef781bafe51d1be2a2551c3de8a7b5038d2546e5
Leandro <alexisleandro@gmail.com>
2018-08-11 18:57:07 +0200

    Tile script.

---
+++ LD42RunningOutOfSpace/Assets/Scripts/Tile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+	public type Type;
+	public Vector2 pos;
+	public state State;
+    
+}
+
+public enum type {
+    empty,
+	mountain,
+    water,
+    field
+}
+
+public enum state {
+	healthy,
+    plant,
+    rabbit,
+    cat,
+    fox,
+    eagle,
+    hunter,
+    ranger
+}
